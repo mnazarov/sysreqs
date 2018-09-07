7fbccc8f4de9f5d89c6e45e483c68f3942d5da08
Maxim Nazarov <maxim.nazarov@openanalytics.eu>
2018-09-07 09:16:49 +0200

    'platform' seems to be not needed for 'pkg' url

--- R/urls.R
+++ R/urls.R
@@ -2,7 +2,7 @@
 sysreqs_base_url <- "https://sysreqs.r-hub.io"
 ## sysreqs_base_url <- "http://localhost:3000" # for local testing
 sysreqs_url <- paste0(sysreqs_base_url, "/map/platform/:platform/:query")
-sysreqs_cran_url <- paste0(sysreqs_base_url, "/pkg/:package/:platform")
+sysreqs_cran_url <- paste0(sysreqs_base_url, "/pkg/:package")
 sysreqs_platform_url <- paste0(sysreqs_base_url, "/platform/get/:platform")
 crandeps_url <- "http://crandeps.r-pkg.org/deps/:packages"
 rversions_url <- "http://rversions.r-pkg.org/:version"
